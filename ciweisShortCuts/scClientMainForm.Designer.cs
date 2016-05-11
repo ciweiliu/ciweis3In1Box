485d9efc919a07ad5520044328cc776cf1a4054e
ciwei <lzwdlut@163.com>
2016-05-11 11:44:20 +0800

    创建库的第一个版本

---
+++ ciweisShortCuts/scClientMainForm.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace ciweisShortCuts
+{
+    partial class scClientMainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.leftPanel = new DevComponents.DotNetBar.PanelEx();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.topSpliter = new DevComponents.DotNetBar.ExpandableSplitter();
+            this.topPanel = new DevComponents.DotNetBar.PanelEx();
+            this.leftSpliter = new DevComponents.DotNetBar.ExpandableSplitter();
+            this.windowsPanel = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // leftPanel
+            // 
+            this.leftPanel.CanvasColor = System.Drawing.SystemColors.Control;
+            this.leftPanel.ColorSchemeStyle = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.leftPanel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.leftPanel.Location = new System.Drawing.Point(356, 0);
+            this.leftPanel.Name = "leftPanel";
+            this.leftPanel.Size = new System.Drawing.Size(280, 347);
+            this.leftPanel.Style.Alignment = System.Drawing.StringAlignment.Center;
+            this.leftPanel.Style.BackColor1.Color = System.Drawing.Color.White;
+            this.leftPanel.Style.BackColor2.Color = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.leftPanel.Style.Border = DevComponents.DotNetBar.eBorderType.SingleLine;
+            this.leftPanel.Style.BorderColor.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder;
+            this.leftPanel.Style.ForeColor.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelText;
+            this.leftPanel.Style.GradientAngle = 90;
+            this.leftPanel.TabIndex = 1;
+            this.leftPanel.Text = "panelEx2";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.windowsPanel);
+            this.panel1.Controls.Add(this.topSpliter);
+            this.panel1.Controls.Add(this.topPanel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(350, 347);
+            this.panel1.TabIndex = 2;
+            // 
+            // topSpliter
+            // 
+            this.topSpliter.BackColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(147)))), ((int)(((byte)(207)))));
+            this.topSpliter.BackColor2SchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder;
+            this.topSpliter.BackColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground;
+            this.topSpliter.Dock = System.Windows.Forms.DockStyle.Top;
+            this.topSpliter.ExpandableControl = this.topPanel;
+            this.topSpliter.ExpandFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(147)))), ((int)(((byte)(207)))));
+            this.topSpliter.ExpandFillColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder;
+            this.topSpliter.ExpandLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.topSpliter.ExpandLineColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.ItemText;
+            this.topSpliter.GripDarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.topSpliter.GripDarkColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.ItemText;
+            this.topSpliter.GripLightColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(239)))), ((int)(((byte)(255)))));
+            this.topSpliter.GripLightColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.BarBackground;
+            this.topSpliter.HotBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(151)))), ((int)(((byte)(61)))));
+            this.topSpliter.HotBackColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(184)))), ((int)(((byte)(94)))));
+            this.topSpliter.HotBackColor2SchemePart = DevComponents.DotNetBar.eColorSchemePart.ItemPressedBackground2;
+            this.topSpliter.HotBackColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.ItemPressedBackground;
+            this.topSpliter.HotExpandFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(147)))), ((int)(((byte)(207)))));
+            this.topSpliter.HotExpandFillColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder;
+            this.topSpliter.HotExpandLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.topSpliter.HotExpandLineColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.ItemText;
+            this.topSpliter.HotGripDarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(147)))), ((int)(((byte)(207)))));
+            this.topSpliter.HotGripDarkColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder;
+            this.topSpliter.HotGripLightColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(239)))), ((int)(((byte)(255)))));
+            this.topSpliter.HotGripLightColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.BarBackground;
+            this.topSpliter.Location = new System.Drawing.Point(0, 80);
+            this.topSpliter.Name = "topSpliter";
+            this.topSpliter.Size = new System.Drawing.Size(350, 6);
+            this.topSpliter.Style = DevComponents.DotNetBar.eSplitterStyle.Office2007;
+            this.topSpliter.TabIndex = 0;
+            this.topSpliter.TabStop = false;
+            // 
+            // topPanel
+            // 
+            this.topPanel.CanvasColor = System.Drawing.SystemColors.Control;
+            this.topPanel.ColorSchemeStyle = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.topPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.topPanel.Location = new System.Drawing.Point(0, 0);
+            this.topPanel.Name = "topPanel";
+            this.topPanel.Size = new System.Drawing.Size(350, 80);
+            this.topPanel.Style.Alignment = System.Drawing.StringAlignment.Center;
+            this.topPanel.Style.BackColor1.Color = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.topPanel.Style.BackColor2.Color = System.Drawing.Color.Red;
+            this.topPanel.Style.Border = DevComponents.DotNetBar.eBorderType.SingleLine;
+            this.topPanel.Style.BorderColor.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder;
+            this.topPanel.Style.ForeColor.ColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelText;
+            this.topPanel.Style.GradientAngle = 90;
+            this.topPanel.TabIndex = 3;
+            this.topPanel.Text = "panelEx1";
+            // 
+            // leftSpliter
+            // 
+            this.leftSpliter.BackColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(147)))), ((int)(((byte)(207)))));
+            this.leftSpliter.BackColor2SchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder;
+            this.leftSpliter.BackColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground;
+            this.leftSpliter.Dock = System.Windows.Forms.DockStyle.Right;
+            this.leftSpliter.ExpandableControl = this.leftPanel;
+            this.leftSpliter.ExpandFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(147)))), ((int)(((byte)(207)))));
+            this.leftSpliter.ExpandFillColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder;
+            this.leftSpliter.ExpandLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.leftSpliter.ExpandLineColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.ItemText;
+            this.leftSpliter.GripDarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.leftSpliter.GripDarkColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.ItemText;
+            this.leftSpliter.GripLightColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(239)))), ((int)(((byte)(255)))));
+            this.leftSpliter.GripLightColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.BarBackground;
+            this.leftSpliter.HotBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(151)))), ((int)(((byte)(61)))));
+            this.leftSpliter.HotBackColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(184)))), ((int)(((byte)(94)))));
+            this.leftSpliter.HotBackColor2SchemePart = DevComponents.DotNetBar.eColorSchemePart.ItemPressedBackground2;
+            this.leftSpliter.HotBackColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.ItemPressedBackground;
+            this.leftSpliter.HotExpandFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(147)))), ((int)(((byte)(207)))));
+            this.leftSpliter.HotExpandFillColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder;
+            this.leftSpliter.HotExpandLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.leftSpliter.HotExpandLineColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.ItemText;
+            this.leftSpliter.HotGripDarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(147)))), ((int)(((byte)(207)))));
+            this.leftSpliter.HotGripDarkColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder;
+            this.leftSpliter.HotGripLightColor = System.Drawing.Color.FromArgb(((int)(((byte)(227)))), ((int)(((byte)(239)))), ((int)(((byte)(255)))));
+            this.leftSpliter.HotGripLightColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.BarBackground;
+            this.leftSpliter.Location = new System.Drawing.Point(350, 0);
+            this.leftSpliter.Name = "leftSpliter";
+            this.leftSpliter.Size = new System.Drawing.Size(6, 347);
+            this.leftSpliter.Style = DevComponents.DotNetBar.eSplitterStyle.Office2007;
+            this.leftSpliter.TabIndex = 5;
+            this.leftSpliter.TabStop = false;
+            // 
+            // windowsPanel
+            // 
+            this.windowsPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.windowsPanel.Location = new System.Drawing.Point(0, 86);
+            this.windowsPanel.Name = "windowsPanel";
+            this.windowsPanel.Size = new System.Drawing.Size(350, 261);
+            this.windowsPanel.TabIndex = 4;
+            // 
+            // scClientMainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 347);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.leftSpliter);
+            this.Controls.Add(this.leftPanel);
+            this.Name = "scClientMainForm";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.scClientMainForm_Load);
+            this.Resize += new System.EventHandler(this.scClientMainForm_Resize);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.PanelEx leftPanel;
+        private System.Windows.Forms.Panel panel1;
+        private DevComponents.DotNetBar.ExpandableSplitter leftSpliter;
+        private DevComponents.DotNetBar.PanelEx topPanel;
+        private DevComponents.DotNetBar.ExpandableSplitter topSpliter;
+        private System.Windows.Forms.Panel windowsPanel;
+
+    }
+}
---
+++ ciweisShortCuts/scClientMainForm.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SmileWei.EmbeddedApp;
+using System.Runtime.InteropServices;
+using System.Threading;
+namespace ciweisShortCuts
+{
+    public partial class scClientMainForm : Form
+    {
+        AppContainer appBox;
+       
+        public scClientMainForm()
+        {
+            InitializeComponent();
+            
+         //   appBox.Width = windowsPanel.Width;
+        //    appBox.Height = windowsPanel.Height;
+        }
+
+        private void scClientMainForm_Resize(object sender, EventArgs e)
+        {
+            var app = appBox.AppProcess;
+            if (app == null) { return; }
+
+            var c = Form.FromHandle(app.MainWindowHandle);
+            var f = c as Form;
+            if (f != null)
+            {
+                Console.WriteLine(f.Parent == null);
+            }
+            appBox.Width = windowsPanel.Width;
+            appBox.Height = windowsPanel.Height;
+        }
+
+        [DllImport("User32.dll", EntryPoint = "FindWindow")]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("User32.dll", EntryPoint = "MoveWindow")]
+        private static extern bool MoveWindow(IntPtr hWnd, int x, int y, int nWidth, int nHeight, bool BRePaint);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern long SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        private const int GWL_STYLE = (-16);
+        private const int WS_VISIBLE = 0x10000000;
+        [DllImport("user32", EntryPoint = "SetWindowLong", SetLastError = true)]
+        private static extern uint SetWindowLong(IntPtr hwnd,int nIndex, uint dwNewLong);
+
+        private void addWindowsToPanel()
+        {
+            appBox.AppFilename = @"C:\Windows\explorer.exe";
+            appBox.Start();
+
+            Thread.Sleep(3000);
+            IntPtr hwnd = new IntPtr(0);
+            hwnd = FindWindow(null, "库");
+            if (hwnd != IntPtr.Zero)
+            {
+                MoveWindow(hwnd, 0, 0, windowsPanel.Width, windowsPanel.Height, true);
+
+                SetParent(hwnd, this.Handle);
+                Win32API.SetWindowLong(new HandleRef(this.appBox, hwnd), GWL_STYLE, WS_VISIBLE);
+            }
+        }
+
+        private void scClientMainForm_Load(object sender, EventArgs e)
+        {
+            appBox = new AppContainer(false);
+            windowsPanel.Controls.Add(appBox);
+            appBox.Location = new Point(windowsPanel.Location.X, windowsPanel.Location.Y);
+            this.appBox.ShowEmbedResult = true;
+
+            addWindowsToPanel();
+        }
+       
+    }
+}
