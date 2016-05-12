24f6ba26753025e591ef830cbfcfb5cdfc05d94a
ciwei <lzwdlut@163.com>
2016-05-12 22:31:06 +0800

    完成资源管理器的集成

---
+++ ciweisShortCuts/exploreOP.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+namespace ciweisShortCuts
+{
+    public class exploreOP
+    {
+        private const int GWL_STYLE = (-16);
+        private const int WS_VISIBLE = 0x10000000;
+
+        public static void addWindowsToPanel(Panel windowsPanel,ref IntPtr hwnd,ref bool isRunning)
+        {
+            //   originalSize.Clear();
+            hwnd = windowsAPI.FindWindow(null, "库");
+            if (hwnd != IntPtr.Zero)
+            {
+                AppEmbeded(hwnd, windowsPanel);
+                windowsAPI.MoveWindow(hwnd, 0, 0, windowsPanel.Width, windowsPanel.Height, true);
+                isRunning = true;               
+            }
+            // winPanelOriginH = windowsPanel.Height;
+            //  winPanelOriginW = windowsPanel.Width;
+        }
+
+        public static void startExe()
+        {
+            string exeName = @"C:\Windows\explorer.exe";
+            Process p = new Process();
+            ProcessStartInfo startinfo = new ProcessStartInfo();//创建进程时使用的一组值，如下面的属性
+
+            startinfo.FileName = exeName;//设定需要执行的命令程序
+            //以下是隐藏cmd窗口的方法
+            startinfo.Arguments = null;//设定参数，要输入到命令程序的字符，其中"/c"表示执行完命令后马上退出
+            startinfo.UseShellExecute = false;//不使用系统外壳程序启动
+            startinfo.RedirectStandardInput = false;//不重定向输入
+            startinfo.RedirectStandardOutput = false;//重定向输出，而不是默认的显示在dos控制台上
+            startinfo.CreateNoWindow = true;//不创建窗口
+            p.StartInfo = startinfo;
+            p.Start();
+        }
+
+        //嵌入APP程序
+        private static void AppEmbeded(IntPtr _handle, Control _control)
+        {
+            //改变父窗口
+            windowsAPI.SetParent(_handle, _control.Handle);
+            windowsAPI.SetWindowLong(_handle, GWL_STYLE, WS_VISIBLE);
+        }
+
+        #region
+        //找到“库”窗口的所有子窗口，form变化时，所有子窗口按比例变化
+        //List<WindowInfo> winList =windowsAPI.EnumChildWindowsCallback(hwnd, string.Empty, string.Empty);
+
+        //foreach (WindowInfo win in winList)
+        //{
+        //    int w = win.szSize.Right - win.szSize.Left;
+        //    int h = win.szSize.Bottom - win.szSize.Top;
+        //    winWH  wh= new winWH(w,h);
+        ////    originalSize.Add(win.hWnd,wh);
+        //}
+        #endregion
+    }
+}
---
+++ ciweisShortCuts/windowsAPI.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace ciweisShortCuts
+{
+    public class windowsAPI
+    {
+        [DllImport("User32.dll", EntryPoint = "FindWindow")]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("User32.dll", EntryPoint = "MoveWindow")]
+        public static extern bool MoveWindow(IntPtr hWnd, int x, int y, int nWidth, int nHeight, bool BRePaint);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern long SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        [DllImport("user32", EntryPoint = "SetWindowLong", SetLastError = true)]
+        public static extern uint SetWindowLong(IntPtr hwnd, int nIndex, uint dwNewLong);
+
+        [DllImport("user32.dll")]
+        public static extern int GetWindowRect(IntPtr hwnd, ref Rect lpRect);
+
+        [DllImport("user32.dll", ExactSpelling = true)]
+        public static extern bool EnumChildWindows(IntPtr hwndParent, WNDENUMPROC lpEnumFunc, int lParam);
+
+        [DllImport("user32.dll")]
+        public static extern int GetWindowTextW(IntPtr hWnd, [MarshalAs(UnmanagedType.LPWStr)]StringBuilder lpString, int nMaxCount);
+        [DllImport("user32.dll")]
+        public static extern int GetClassNameW(IntPtr hWnd, [MarshalAs(UnmanagedType.LPWStr)]StringBuilder lpString, int nMaxCount);
+
+        [DllImport("user32.dll")]
+        public static extern bool InvalidateRect(int hWnd, ref Rect lpRect, bool bErase);
+
+        public delegate bool WNDENUMPROC(IntPtr hWnd, int lParam);
+
+        public static List<WindowInfo> EnumChildWindowsCallback(IntPtr handle, string name, string classname)
+        {
+            List<WindowInfo> wndList = new List<WindowInfo>();
+
+            EnumChildWindows(handle, delegate(IntPtr hWnd, int lParam)
+            {
+                WindowInfo wnd = new WindowInfo();
+                StringBuilder sb = new StringBuilder(256);
+                //get hwnd 
+                wnd.hWnd = hWnd;
+                //get window name 
+                GetWindowTextW(hWnd, sb, sb.Capacity);
+                wnd.szWindowName = sb.ToString();
+                //get window class 
+                GetClassNameW(hWnd, sb, sb.Capacity);
+                wnd.szClassName = sb.ToString();
+
+                // get window size
+                Rect rec = new Rect();
+                GetWindowRect(hWnd, ref rec);
+                wnd.szSize = rec;
+
+                //add it into list 
+                wndList.Add(wnd);
+                return true;
+            }, 0);
+            return wndList;
+            // return wndList.Where(it => it.szWindowName == name && it.szClassName == classname).ToList();
+        }
+    }
+
+    public struct WindowInfo
+    {
+        public IntPtr hWnd;
+        public string szWindowName;
+        public string szClassName;
+        public Rect szSize;
+    }
+
+    public struct Rect
+    {
+        public int Left;
+        public int Top;
+        public int Right;
+        public int Bottom;
+    }
+
+    public class winWH
+    {
+        public winWH(int w, int h)
+        {
+            _w = w;
+            _h = h;
+        }
+        int _w;
+        int _h;
+        public int Width { get { return _w; } }
+        public int Height { get { return _h; } }
+    }
+}
